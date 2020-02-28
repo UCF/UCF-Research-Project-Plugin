a07fff2d8efe070390d1f66a949e419a0bfc1aa4
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-02-28 12:07:26 -0500

    Updated project names and descriptions

---
+++ ucf-research.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: UCF Research Plugin
+Description: The UCF Research Plugin provides a custom post type and fields to describe areas of research being conducted at UCF.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/UCF-Research-Plugin
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
